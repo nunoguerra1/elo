7bb2b537b60be7be524de64f35e80c75131142f4
nunoguerra1 <nunomiguel@alunos.utfpr.edu.br>
2026-09-07 18:27:59 -0300

    feat: add motion primitives (reveal, magnetic button, word reveal)

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,1 +1,7 @@
-export { cn } from "cn"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+// Combina classes condicionais e resolve conflitos do Tailwind
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
